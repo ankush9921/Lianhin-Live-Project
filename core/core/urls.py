50bb61eccf6a759cbdebc3ddb0deee05f37f0bc9
Amittagde <124119508+Amittagde@users.noreply.github.com>
2023-09-20 15:45:46 +0530

    updates

--- core/core/urls.py
+++ core/core/urls.py
@@ -20,5 +20,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('lianhin.urls'))
+    path('',include('lianhin.urls')),
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
--- core/lianhin/urls.py
+++ core/lianhin/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .import views
 
+handler404 = views.error_404_view
+
 
 urlpatterns = [
     path('',views.sign_in,name='sign_in'),
